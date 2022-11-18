d88988c6348710e91558a0dea8ce82c6b659177b
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-11-18 17:39:11 -0500

    Removed comments and added transitions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import './style.css';
 import { postScore, displayScores } from './modules/game.js';
 
-// Event listener to add a score
 document.querySelector('#add-score').addEventListener('click', (e) => {
   e.preventDefault();
   postScore();
 });
 
-// Event listener to show stored scores on click of refresh
 document.querySelector('#refresh').addEventListener('click', () => {
   displayScores();
 });
 
-// Event listener to show stored scores on page refresh
 document.addEventListener('DOMContentLoaded', displayScores());
